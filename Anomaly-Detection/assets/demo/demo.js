a150cc924ac3262ae7a5019e1779487b427598f3
Hoàng Tùng <54903350+hoangtung97@users.noreply.github.com>
2020-03-07 10:52:59 +0700

    Commit

--- Anomaly-Detection/assets/demo/demo.js
+++ Anomaly-Detection/assets/demo/demo.js
@@ -86,53 +86,53 @@ demo = {
 
   initDashboardPageCharts: function() {
 
-    gradientChartOptionsConfigurationWithTooltipBlue = {
-      maintainAspectRatio: false,
-      legend: {
-        display: false
-      },
+    // gradientChartOptionsConfigurationWithTooltipBlue = {
+    //   maintainAspectRatio: false,
+    //   legend: {
+    //     display: false
+    //   },
 
-      tooltips: {
-        backgroundColor: '#f5f5f5',
-        titleFontColor: '#333',
-        bodyFontColor: '#666',
-        bodySpacing: 4,
-        xPadding: 12,
-        mode: "nearest",
-        intersect: 0,
-        position: "nearest"
-      },
-      responsive: true,
-      scales: {
-        yAxes: [{
-          barPercentage: 1.6,
-          gridLines: {
-            drawBorder: false,
-            color: 'rgba(29,140,248,0.0)',
-            zeroLineColor: "transparent",
-          },
-          ticks: {
-            suggestedMin: 60,
-            suggestedMax: 125,
-            padding: 20,
-            fontColor: "#2380f7"
-          }
-        }],
+    //   tooltips: {
+    //     backgroundColor: '#f5f5f5',
+    //     titleFontColor: '#333',
+    //     bodyFontColor: '#666',
+    //     bodySpacing: 4,
+    //     xPadding: 12,
+    //     mode: "nearest",
+    //     intersect: 0,
+    //     position: "nearest"
+    //   },
+    //   responsive: true,
+    //   scales: {
+    //     yAxes: [{
+    //       barPercentage: 1.6,
+    //       gridLines: {
+    //         drawBorder: false,
+    //         color: 'rgba(29,140,248,0.0)',
+    //         zeroLineColor: "transparent",
+    //       },
+    //       ticks: {
+    //         suggestedMin: 60,
+    //         suggestedMax: 125,
+    //         padding: 20,
+    //         fontColor: "#2380f7"
+    //       }
+    //     }],
 
-        xAxes: [{
-          barPercentage: 1.6,
-          gridLines: {
-            drawBorder: false,
-            color: 'rgba(29,140,248,0.1)',
-            zeroLineColor: "transparent",
-          },
-          ticks: {
-            padding: 20,
-            fontColor: "#2380f7"
-          }
-        }]
-      }
-    };
+    //     xAxes: [{
+    //       barPercentage: 1.6,
+    //       gridLines: {
+    //         drawBorder: false,
+    //         color: 'rgba(29,140,248,0.1)',
+    //         zeroLineColor: "transparent",
+    //       },
+    //       ticks: {
+    //         padding: 20,
+    //         fontColor: "#2380f7"
+    //       }
+    //     }]
+    //   }
+    // };
 
     gradientChartOptionsConfigurationWithTooltipPurple = {
       maintainAspectRatio: false,
@@ -230,102 +230,102 @@ demo = {
       }
     };
 
-    gradientChartOptionsConfigurationWithTooltipGreen = {
-      maintainAspectRatio: false,
-      legend: {
-        display: false
-      },
+    // gradientChartOptionsConfigurationWithTooltipGreen = {
+    //   maintainAspectRatio: false,
+    //   legend: {
+    //     display: false
+    //   },
 
-      tooltips: {
-        backgroundColor: '#f5f5f5',
-        titleFontColor: '#333',
-        bodyFontColor: '#666',
-        bodySpacing: 4,
-        xPadding: 12,
-        mode: "nearest",
-        intersect: 0,
-        position: "nearest"
-      },
-      responsive: true,
-      scales: {
-        yAxes: [{
-          barPercentage: 1.6,
-          gridLines: {
-            drawBorder: false,
-            color: 'rgba(29,140,248,0.0)',
-            zeroLineColor: "transparent",
-          },
-          ticks: {
-            suggestedMin: 50,
-            suggestedMax: 125,
-            padding: 20,
-            fontColor: "#9e9e9e"
-          }
-        }],
+    //   tooltips: {
+    //     backgroundColor: '#f5f5f5',
+    //     titleFontColor: '#333',
+    //     bodyFontColor: '#666',
+    //     bodySpacing: 4,
+    //     xPadding: 12,
+    //     mode: "nearest",
+    //     intersect: 0,
+    //     position: "nearest"
+    //   },
+    //   responsive: true,
+    //   scales: {
+    //     yAxes: [{
+    //       barPercentage: 1.6,
+    //       gridLines: {
+    //         drawBorder: false,
+    //         color: 'rgba(29,140,248,0.0)',
+    //         zeroLineColor: "transparent",
+    //       },
+    //       ticks: {
+    //         suggestedMin: 50,
+    //         suggestedMax: 125,
+    //         padding: 20,
+    //         fontColor: "#9e9e9e"
+    //       }
+    //     }],
 
-        xAxes: [{
-          barPercentage: 1.6,
-          gridLines: {
-            drawBorder: false,
-            color: 'rgba(0,242,195,0.1)',
-            zeroLineColor: "transparent",
-          },
-          ticks: {
-            padding: 20,
-            fontColor: "#9e9e9e"
-          }
-        }]
-      }
-    };
+    //     xAxes: [{
+    //       barPercentage: 1.6,
+    //       gridLines: {
+    //         drawBorder: false,
+    //         color: 'rgba(0,242,195,0.1)',
+    //         zeroLineColor: "transparent",
+    //       },
+    //       ticks: {
+    //         padding: 20,
+    //         fontColor: "#9e9e9e"
+    //       }
+    //     }]
+    //   }
+    // };
 
 
-    gradientBarChartConfiguration = {
-      maintainAspectRatio: false,
-      legend: {
-        display: false
-      },
+    // gradientBarChartConfiguration = {
+    //   maintainAspectRatio: false,
+    //   legend: {
+    //     display: false
+    //   },
 
-      tooltips: {
-        backgroundColor: '#f5f5f5',
-        titleFontColor: '#333',
-        bodyFontColor: '#666',
-        bodySpacing: 4,
-        xPadding: 12,
-        mode: "nearest",
-        intersect: 0,
-        position: "nearest"
-      },
-      responsive: true,
-      scales: {
-        yAxes: [{
+    //   tooltips: {
+    //     backgroundColor: '#f5f5f5',
+    //     titleFontColor: '#333',
+    //     bodyFontColor: '#666',
+    //     bodySpacing: 4,
+    //     xPadding: 12,
+    //     mode: "nearest",
+    //     intersect: 0,
+    //     position: "nearest"
+    //   },
+    //   responsive: true,
+    //   scales: {
+    //     yAxes: [{
 
-          gridLines: {
-            drawBorder: false,
-            color: 'rgba(29,140,248,0.1)',
-            zeroLineColor: "transparent",
-          },
-          ticks: {
-            suggestedMin: 60,
-            suggestedMax: 120,
-            padding: 20,
-            fontColor: "#9e9e9e"
-          }
-        }],
+    //       gridLines: {
+    //         drawBorder: false,
+    //         color: 'rgba(29,140,248,0.1)',
+    //         zeroLineColor: "transparent",
+    //       },
+    //       ticks: {
+    //         suggestedMin: 60,
+    //         suggestedMax: 120,
+    //         padding: 20,
+    //         fontColor: "#9e9e9e"
+    //       }
+    //     }],
 
-        xAxes: [{
+    //     xAxes: [{
 
-          gridLines: {
-            drawBorder: false,
-            color: 'rgba(29,140,248,0.1)',
-            zeroLineColor: "transparent",
-          },
-          ticks: {
-            padding: 20,
-            fontColor: "#9e9e9e"
-          }
-        }]
-      }
-    };
+    //       gridLines: {
+    //         drawBorder: false,
+    //         color: 'rgba(29,140,248,0.1)',
+    //         zeroLineColor: "transparent",
+    //       },
+    //       ticks: {
+    //         padding: 20,
+    //         fontColor: "#9e9e9e"
+    //       }
+    //     }]
+    //   }
+    // };
 
     var ctx = document.getElementById("chartLinePurple").getContext("2d");
 
@@ -365,41 +365,41 @@ demo = {
 
      
   
-    var ctxGreen = document.getElementById("chartLineGreen").getContext("2d");
+    // var ctxGreen = document.getElementById("chartLineGreen").getContext("2d");
 
-    var gradientStroke = ctx.createLinearGradient(0, 230, 0, 50);
+    // var gradientStroke = ctx.createLinearGradient(0, 230, 0, 50);
 
-    gradientStroke.addColorStop(1, 'rgba(66,134,121,0.15)');
-    gradientStroke.addColorStop(0.4, 'rgba(66,134,121,0.0)'); //green colors
-    gradientStroke.addColorStop(0, 'rgba(66,134,121,0)'); //green colors
+    // gradientStroke.addColorStop(1, 'rgba(66,134,121,0.15)');
+    // gradientStroke.addColorStop(0.4, 'rgba(66,134,121,0.0)'); //green colors
+    // gradientStroke.addColorStop(0, 'rgba(66,134,121,0)'); //green colors
 
-    var data = {
-      labels: ['JUL', 'AUG', 'SEP', 'OCT', 'NOV'],
-      datasets: [{
-        label: "My First dataset",
-        fill: true,
-        backgroundColor: gradientStroke,
-        borderColor: '#00d6b4',
-        borderWidth: 2,
-        borderDash: [],
-        borderDashOffset: 0.0,
-        pointBackgroundColor: '#00d6b4',
-        pointBorderColor: 'rgba(255,255,255,0)',
-        pointHoverBackgroundColor: '#00d6b4',
-        pointBorderWidth: 20,
-        pointHoverRadius: 4,
-        pointHoverBorderWidth: 15,
-        pointRadius: 4,
-        data: [90, 27, 60, 12, 80],
-      }]
-    };
+    // var data = {
+    //   labels: ['JUL', 'AUG', 'SEP', 'OCT', 'NOV'],
+    //   datasets: [{
+    //     label: "My First dataset",
+    //     fill: true,
+    //     backgroundColor: gradientStroke,
+    //     borderColor: '#00d6b4',
+    //     borderWidth: 2,
+    //     borderDash: [],
+    //     borderDashOffset: 0.0,
+    //     pointBackgroundColor: '#00d6b4',
+    //     pointBorderColor: 'rgba(255,255,255,0)',
+    //     pointHoverBackgroundColor: '#00d6b4',
+    //     pointBorderWidth: 20,
+    //     pointHoverRadius: 4,
+    //     pointHoverBorderWidth: 15,
+    //     pointRadius: 4,
+    //     data: [90, 27, 60, 12, 80],
+    //   }]
+    // };
 
-    var myChart = new Chart(ctxGreen, {
-      type: 'line',
-      data: data,
-      options: gradientChartOptionsConfigurationWithTooltipGreen
+    // var myChart = new Chart(ctxGreen, {
+    //   type: 'line',
+    //   data: data,
+    //   options: gradientChartOptionsConfigurationWithTooltipGreen
 
-    });
+    // });
 
 
 
@@ -446,36 +446,36 @@ demo = {
       myChartData.update();
     });
 
-    var ctx = document.getElementById("CountryChart").getContext("2d");
+    // var ctx = document.getElementById("CountryChart").getContext("2d");
 
-    var gradientStroke = ctx.createLinearGradient(0, 230, 0, 50);
+    // var gradientStroke = ctx.createLinearGradient(0, 230, 0, 50);
 
-    gradientStroke.addColorStop(1, 'rgba(29,140,248,0.2)');
-    gradientStroke.addColorStop(0.4, 'rgba(29,140,248,0.0)');
-    gradientStroke.addColorStop(0, 'rgba(29,140,248,0)'); //blue colors
+    // gradientStroke.addColorStop(1, 'rgba(29,140,248,0.2)');
+    // gradientStroke.addColorStop(0.4, 'rgba(29,140,248,0.0)');
+    // gradientStroke.addColorStop(0, 'rgba(29,140,248,0)'); //blue colors
 
 
-    var myChart = new Chart(ctx, {
-      type: 'bar',
-      responsive: true,
-      legend: {
-        display: false
-      },
-      data: {
-        labels: ['USA', 'GER', 'AUS', 'UK', 'RO', 'BR'],
-        datasets: [{
-          label: "Countries",
-          fill: true,
-          backgroundColor: gradientStroke,
-          hoverBackgroundColor: gradientStroke,
-          borderColor: '#1f8ef1',
-          borderWidth: 2,
-          borderDash: [],
-          borderDashOffset: 0.0,
-          data: [53, 20, 10, 80, 100, 45],
-        }]
-      },
-      options: gradientBarChartConfiguration
-    });
+    // var myChart = new Chart(ctx, {
+    //   type: 'bar',
+    //   responsive: true,
+    //   legend: {
+    //     display: false
+    //   },
+    //   data: {
+    //     labels: ['USA', 'GER', 'AUS', 'UK', 'RO', 'BR'],
+    //     datasets: [{
+    //       label: "Countries",
+    //       fill: true,
+    //       backgroundColor: gradientStroke,
+    //       hoverBackgroundColor: gradientStroke,
+    //       borderColor: '#1f8ef1',
+    //       borderWidth: 2,
+    //       borderDash: [],
+    //       borderDashOffset: 0.0,
+    //       data: [53, 20, 10, 80, 100, 45],
+    //     }]
+    //   },
+    //   options: gradientBarChartConfiguration
+    // });
   },
 };
