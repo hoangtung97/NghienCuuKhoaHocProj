5e64c775f8a8d8db6242693d78fcb87bd6d236cd
Hoàng Tùng <54903350+hoangtung97@users.noreply.github.com>
2020-03-27 22:12:59 +0700

    tung

--- Anomaly-Detection/assets/demo/anomalydetection.js
+++ Anomaly-Detection/assets/demo/anomalydetection.js
@@ -1,5 +1,4 @@
 
-// Themes begin
 am4core.useTheme(am4themes_animated);
 // Themes end
 
@@ -7,75 +6,66 @@ am4core.useTheme(am4themes_animated);
 var chart = am4core.create("chartdiv", am4charts.XYChart);
 
 // Add data
-chart.data = generateChartData();
+chart.data = generatechartData();
+function generatechartData() {
+  var chartData = [];
+  var firstDate = new Date();
+  firstDate.setDate( firstDate.getDate() - 150 );
+  var visits = 50;
+  var b = 0.6;
+  for ( var i = 0; i < 150; i++ ) {
+    var newDate = new Date( firstDate );
+    newDate.setDate( newDate.getDate() + i );
+    if(i > 80){
+        b = 0.4;
+    }
+    visits += Math.round((Math.random()<b?1:-1)*Math.random()*10);
+
+    chartData.push( {
+      date: newDate,
+      visits: visits
+    } );
+  }
+  return chartData;
+}
 
 // Create axes
 var dateAxis = chart.xAxes.push(new am4charts.DateAxis());
-dateAxis.renderer.minGridDistance = 50;
+dateAxis.startLocation = 0.5;
+dateAxis.endLocation = 0.5;
 
+// Create value axis
 var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
 
 // Create series
 var series = chart.series.push(new am4charts.LineSeries());
 series.dataFields.valueY = "visits";
 series.dataFields.dateX = "date";
-series.strokeWidth = 2;
-series.minBulletDistance = 10;
-series.tooltipText = "{valueY}";
-series.tooltip.pointerOrientation = "vertical";
-series.tooltip.background.cornerRadius = 20;
-series.tooltip.background.fillOpacity = 0.5;
-series.tooltip.label.padding(12,12,12,12)
+series.strokeWidth = 3;
+series.tooltipText = "{valueY.value}";
+series.fillOpacity = 0.1;
 
-
-//danh dau dinh khi zoom chart
-series.tooltip.background.cornerRadius = 20;
-series.tooltip.background.strokeOpacity = 0;
-series.tooltip.pointerOrientation = "vertical";
-series.tooltip.label.minWidth = 40;
-series.tooltip.label.minHeight = 40;
-series.tooltip.label.textAlign = "middle";
-series.tooltip.label.textValign = "middle";
-
-
-var bullet = series.bullets.push(new am4charts.CircleBullet());
-bullet.circle.strokeWidth = 2;
-bullet.circle.radius = 4;
-bullet.circle.fill = am4core.color("#fff");
-
-var bullethover = bullet.states.create("hover");
-bullethover.properties.scale = 1.3;
-
-// Zoom truc Y
-chart.scrollbarY = new am4core.Scrollbar();
-chart.scrollbarY.parent = chart.leftAxesContainer;
-chart.scrollbarY.toBack();
-
-// Zoom truc X
-chart.scrollbarX = new am4charts.XYChartScrollbar();
-chart.scrollbarX.series.push(series);
+// Create a range to change stroke for values below 0
+var range = valueAxis.createSeriesRange(series);
+range.value = 60;
+range.endValue = -1000;
+range.contents.stroke = chart.colors.getIndex(5);
+range.contents.fill = range.contents.stroke;
+range.contents.strokeOpacity = 0.7;
+range.contents.fillOpacity = 0.1;
 
 // Add cursor
 chart.cursor = new am4charts.XYCursor();
 chart.cursor.xAxis = dateAxis;
-chart.cursor.snapToSeries = series;
+chart.scrollbarX = new am4core.Scrollbar();
 
-function generateChartData() {
-    var chartData = [];
-    var firstDate = new Date();
-    firstDate.setDate(firstDate.getDate() - 1000);
-    var visits = 1200;
-    for (var i = 0; i < 500; i++) {
-        var newDate = new Date(firstDate);
-        newDate.setDate(newDate.getDate() + i);
-        
-        visits += Math.round((Math.random()<0.5?1:-1)*Math.random()*10);
-
-        chartData.push({
-            type: "line",
-            date: newDate,
-            visits: visits
-        });
+series.tooltip.getFillFromObject = false;
+series.tooltip.adapter.add("x", (x, target)=>{
+    if(series.tooltip.tooltipDataItem.valueY < 10){
+        series.tooltip.background.fill = chart.colors.getIndex(5);
     }
-    return chartData;
-}
+    else{
+        series.tooltip.background.fill = chart.colors.getIndex(0);
+    }
+    return x;
+})
