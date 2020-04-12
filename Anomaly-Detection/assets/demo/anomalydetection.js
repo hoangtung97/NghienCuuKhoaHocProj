9a0ab4f4bd4aa424a9d7353e6edb0b97f094deff
Hoàng Tùng <54903350+hoangtung97@users.noreply.github.com>
2020-04-12 19:14:14 +0700

    ggg

--- Anomaly-Detection/assets/demo/anomalydetection.js
+++ Anomaly-Detection/assets/demo/anomalydetection.js
@@ -1,4 +1,5 @@
 
+// Themes begin
 am4core.useTheme(am4themes_animated);
 // Themes end
 
@@ -13,7 +14,7 @@ function generatechartData() {
   firstDate.setDate( firstDate.getDate() - 150 );
   var visits = 50;
   var b = 0.6;
-  for ( var i = 0; i < 150; i++ ) {
+  for ( var i = 0; i < 100; i++ ) {
     var newDate = new Date( firstDate );
     newDate.setDate( newDate.getDate() + i );
     if(i > 80){
@@ -30,11 +31,14 @@ function generatechartData() {
 }
 
 // Create axes
-var dateAxis = chart.xAxes.push(new am4charts.DateAxis());  
-dateAxis.startLocation = 0.5;
-dateAxis.endLocation = 0.5;
+var dateAxis = chart.xAxes.push(new am4charts.DateAxis());
+dateAxis.renderer.minGridDistance = 50;
+dateAxis.renderer.grid.template.location = 0.5;
+dateAxis.baseInterval = {
+  count: 1,
+  timeUnit: "date"
+}
 
-// Create value axis
 var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
 
 // Create series
@@ -42,39 +46,32 @@ var series = chart.series.push(new am4charts.LineSeries());
 series.dataFields.valueY = "visits";
 series.dataFields.dateX = "date";
 series.strokeWidth = 3;
-series.tooltipText = "{valueY.value}";
-series.fillOpacity = 0.1;
-
-// Make bullets grow on hover
+series.connect = false;
+series.tensionX = 0.8;
+series.fillOpacity = 0.2;
 var bullet = series.bullets.push(new am4charts.CircleBullet());
-bullet.circle.strokeWidth = 2;
-bullet.circle.radius = 3;
-bullet.circle.fill = am4core.color("#fff");
+bullet.stroke = new am4core.InterfaceColorSet().getFor("background");
+bullet.strokeWidth = 2;
+bullet.tooltipText = "{valueY}";
+bullet.circle.radius = 4;
 
-var bullethover = bullet.states.create("hover");
-bullethover.properties.scale = 1.3;
 
-// tao duong ranh gioi
-var range = valueAxis.createSeriesRange(series);
-range.value = 80;
-range.endValue = -1000;
-range.contents.stroke = chart.colors.getIndex(5);
-range.contents.fill = range.contents.stroke;
-range.contents.strokeOpacity = 0.7;
-range.contents.fillOpacity = 0.1;
-
-// Add cursor
-chart.cursor = new am4charts.XYCursor();
-chart.cursor.xAxis = dateAxis;
-chart.scrollbarX = new am4core.Scrollbar();
-
-series.tooltip.getFillFromObject = false;
-series.tooltip.adapter.add("x", (x, target)=>{
-    if(series.tooltip.tooltipDataItem.valueY < 10){
-        series.tooltip.background.fill = chart.colors.getIndex(5);
+chart.colors.list = [
+  am4core.color("#D65DB1"),
+];
+bullet.adapter.add("fill", function(fill, target){
+    if(target.dataItem.valueY > 100){
+        return chart.colors.getIndex(2);
     }
-    else{
-        series.tooltip.background.fill = chart.colors.getIndex(0);
-    }
-    return x;
+    return fill;
 })
+
+var range = valueAxis.createSeriesRange(series);
+range.value = 100;
+range.endValue = 5000;
+range.contents.stroke = chart.colors.getIndex(2);
+range.contents.fill = range.contents.stroke;
+range.contents.fillOpacity = 0.2;
+
+chart.scrollbarX = new am4core.Scrollbar();
+chart.cursor = new am4charts.XYCursor()
